99a359905462cc646a00a53e881f9a8863006839
Anton <ka4kin.anton@yandex.ru>
2024-04-03 09:28:23 +0300

    Car representation
    add car in app

---
+++ src/main/java/web/dao/CarDaoImp.java
@@ -0,0 +1,32 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CarDaoImp implements CarDao{
+
+    private static List<Car> carList = new ArrayList<>();
+    static  {
+        carList.add(new Car ("Toyota",  100, "red"));
+        carList.add(new Car ("BMW",  200, "black"));
+        carList.add(new Car ("Mercedes",  300, "blue"));
+        carList.add(new Car ("Reno",  400, "white"));
+        carList.add(new Car ("Kia",  500, "yellow"));
+
+
+    }
+
+    @Override
+    public List<Car> getCars() {
+        return carList;
+    }
+
+    @Override
+    public List<Car> getCars(int carCount) {
+        return carList.stream().limit(carCount).toList();
+    }
+}
---
+++ src/main/java/web/service/CarServiceImp.java
@@ -0,0 +1,28 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.CarDao;
+import web.model.Car;
+
+import java.util.List;
+
+@Service
+public class CarServiceImp implements CarService {
+    private CarDao carDao;
+
+    @Autowired
+    public CarServiceImp(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+    @Override
+    public List<Car> getCars() {
+        return carDao.getCars();
+    }
+
+    @Override
+    public List<Car> getCars(int carCount) {
+        return carDao.getCars(carCount);
+    }
+}
